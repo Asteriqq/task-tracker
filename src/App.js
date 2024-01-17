7e28f3334156cfea8d2e8f39332893c2f590e6f6
Asteriqq <shayofunmifemi@gmail.com>
2024-01-17 12:49:50 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,83 +1,67 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './component/Header'
-import Footer from './component/Footer'
-import Tasks from './component/Tasks'
-import AddTask from './component/AddTask'
-import About from './component/About'
-
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Tasks from './component/Tasks';
+import AddTask from './component/AddTask';
+import About from './component/About';
 
 const App = () => {
-const [showAddTask, setShowAddTask] = useState(false)
-const [tasks, setTasks] = useState([])
-useEffect(() => {
-const getTasks = async () => {
-const tasksFromServer = await fetchTasks()
-setTasks(tasksFromServer) }
-getTasks() }, [])
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
 
+  // Fetch tasks from local storage on component mount
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks);
+  }, []);
 
-//fetch
-const fetchTasks = async () => {
-const res = await fetch('http://localhost:5000/tasks')
-const data = await res.json()
-return data }
+  // Save tasks to local storage whenever tasks change
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
-const fetchTask = async (id) => {
-  const res = await fetch(`http://localhost:5000/tasks/${id}`)
-  const data = await res.json()
-return data }
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
 
-const addTask = async (task) => {
-const res = await fetch('http://localhost:5000/tasks', { method: 'POST',
-headers: {
-'Content-type': 'application/json', },
-body: JSON.stringify(task), })
-const data = await res.json() 
-setTasks([...tasks, data])
-}
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
+  const toggleReminder = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
-//delete
-const deleteTask = async (id) => {
-await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE',
-})
+  return (
+    <Router>
+      <div className='container'>
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
 
-? setTasks(tasks.filter((task) => task.id !== id))
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            }
+          />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
- : alert('Error Deleting This Task') }
-
-const toggleReminder = async (id) => {
-const taskToToggle = await fetchTask(id)
-const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
-const res = await fetch(`http://localhost:5000/tasks/${id}`, { method: 'PUT',
-headers: {
-'Content-type': 'application/json', },
-body: JSON.stringify(updTask), })
-const data = await res.json()
-setTasks( tasks.map((task) =>
-task.id === id ? { ...task, reminder: data.reminder } : task )
-) }
-
-return ( <Router>
-<div className='container'> <Header
-onAdd={() => setShowAddTask(!showAddTask)}
-showAdd={showAddTask} />
-
-<Routes> <Route
-path='/' element={
-<>
-{showAddTask && <AddTask onAdd={addTask} />} {tasks.length > 0 ? (
-<Tasks
-tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}
-/>
-
- ):(
-'No Tasks To Show'
-)} </>
-} />
-<Route path='/about' element={<About />} /> </Routes>
-<Footer />
-</div> </Router>
-) }
-export default App
+export default App;
